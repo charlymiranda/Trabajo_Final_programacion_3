c6128e937c9decca68179e3e20e8ba3985826de4
Exeziel <exeziel@hotmail.com>
2021-05-20 21:00:04 -0300

    Cargue datos de Employee y User

--- Hotel Enterprise/src/com/hotelenterprise/users/Employee.java
+++ Hotel Enterprise/src/com/hotelenterprise/users/Employee.java
@@ -1,4 +1,75 @@
 package com.hotelenterprise.users;
 
-public class Employee {
+public abstract class Employee extends User {
+    private int idEmployee;
+    private String permissions;
+    private String userName;
+    private String password;
+    private boolean status;
+
+
+    public Employee() {
+    }
+
+    public Employee(String name, String lastname, String docType, String docNumber, String telephone, String address, String eMail, int idEmployee, String permissions, String userName, String password, boolean status) {
+        super(name, lastname, docType, docNumber, telephone, address, eMail);
+        this.idEmployee = idEmployee;
+        this.permissions = permissions;
+        this.userName = userName;
+        this.password = password;
+        this.status = status;
+    }
+
+    public int getIdEmployee() {
+        return idEmployee;
+    }
+
+    public void setIdEmployee(int idEmployee) {
+        this.idEmployee = idEmployee;
+    }
+
+    public String getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(String permissions) {
+        this.permissions = permissions;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Employee{" + super.toString() +
+                "idEmployee=" + idEmployee +
+                ", permissions='" + permissions + '\'' +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
+
