015bf6880930f4bfb2e8e8fa0aa6b71d502ba388
Carlos Miranda <mirandacarlosmi@gmail.com>
2021-05-23 18:29:26 -0300

    Agregue clases finales
    agregue clases finnales para poder tenre los precios de las habitaciones y los tipos, tambien agregue la clase Console para no tener que hacer un Scaner en cada lugar

---
+++ Hotel Enterprise/src/com/hotelenterprise/functionality/RoomType.java
@@ -0,0 +1,14 @@
+package com.hotelenterprise.functionality;
+
+public final class RoomType {
+
+    public static final String SINGLE = "Room for one";
+    public static final String DOUBLE = "Room for two";
+    public static final String TRIPLE = "Room for three";
+    public static final String QUADRUPLE = "Room for four";
+    public static final String QUINTUPLE = "Room for five";
+    public RoomType(){
+
+    }
+
+}
