180e852873696c15761abf2d06f4cee6d1713840
Carlos Miranda <mirandacarlosmi@gmail.com>
2021-06-01 06:09:29 -0300

    gson
    volvi a agregar el gson y pruebas en el main

--- Hotel Enterprise/src/com/hotelenterprise/Main.java
+++ Hotel Enterprise/src/com/hotelenterprise/Main.java
@@ -1,11 +1,12 @@
 package com.hotelenterprise;
 
-import com.hotelenterprise.functionality.TypeOfRoom;
+
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
 
     }
 }
